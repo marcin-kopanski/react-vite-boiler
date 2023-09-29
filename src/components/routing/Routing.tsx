65b863e95c071366878b1ee7044d95c13d22f5ff
Marcin Kopański <marcin.kopanski@gmail.com>
2023-09-29 14:28:25 +0200

    feat(router, app): react router stub

---
+++ src/components/routing/Routing.tsx
@@ -0,0 +1,3 @@
+export const Routing = () => {
+  return <>Routing</>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
+import { Routing } from './components';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <App>
+      <Routing />
+    </App>
   </React.StrictMode>,
 );
